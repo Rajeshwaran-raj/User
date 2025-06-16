ec7949e7b02867f8070694af1623c3c1823c81a0
Rajeshwaran Murugesan <rajeshwaran.m@payorch.com>
2025-06-16 16:13:31 +0530

    new files added

---
+++ config/db.config.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+console.info('Using DB password from db.config.js file', process.env.DB_PASSWORD);
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  local: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  }
+};
+

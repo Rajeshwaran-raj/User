a2dcba0757888fc94db1f31b929508e051d9e7e0
Rajeshwaran Murugesan <rajeshwaran.m@payorch.com>
2026-01-06 10:30:22 +0530

    changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./app');
 const { name } = require("./package.json");
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 7777;
 
 app.listen(port, () => {
   console.log(`**** ApplicationServer ${name.toUpperCase()} started running on port ${port}****`);
